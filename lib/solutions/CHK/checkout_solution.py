f078a3c256779035eb9e257abc38dd8ab67fd562
sbx_user1051 <sbx_user1051@169.254.34.153>
2022-05-10 18:21:06 +0000

    Tue May 10 18:21:06 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,8 +2,21 @@ import collections
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+'''
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+'''
 def checkout(skus):
     ascii_check = range(65, 69)
+
+    result = 0
 
     if not isinstance(skus, str):
         return -1
@@ -17,10 +30,22 @@ def checkout(skus):
             print(ord(i))
             return -1
 
+        print(table[i])
 
-    print(table)
 
-print(checkout('ABCD'))
+
+
+
+
+print(checkout('ABC'))
+
+
+
+
+
+
+
+
 
 
 
