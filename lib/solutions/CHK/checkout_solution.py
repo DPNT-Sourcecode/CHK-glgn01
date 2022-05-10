8c137ca1f84c27b7c12ca282b37a71aa5da4abfb
sbx_user1051 <sbx_user1051@169.254.34.153>
2022-05-10 19:04:26 +0000

    Tue May 10 19:04:26 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -25,7 +25,7 @@ def checkout(skus):
     result = 0
 
     hashtable = {
-        'A': 50, 'AAA' : 130, 'AAAAA' : 200,'B': 30, 'BB' : 45, 'C': 20, 'D': 15, 'E': 40
+        'A':[(1, 50), (3, 130), (5, 200)], 'B': [(1, 30), (2, 45)], 'C': [(1, 20)], 'D': [(1, 15)], 'E': [(1, 40)]
     }
 
     if not isinstance(skus, str) or not skus.isupper():
@@ -37,43 +37,48 @@ def checkout(skus):
         if ord(i) not in ascii_check:
             return -1
 
-        if i == 'A':
-            mul, res = divmod(table[i], 3)
-            if mul > 0:
-                result += (mul * hashtable[i * 3])
-            if res > 0:
-                result += (res * hashtable[i])
+        for cur in hashtable[i][::-1]:
+            print(cur)
 
-        elif i == 'B':
-            mul, res = divmod(table[i], 2)
-            if mul > 0:
-                result += (mul * hashtable[i*2])
-            if res > 0:
-                result += (res * hashtable[i])
 
-        elif i == 'E':
-            mul, res = divmod(table[i], 2)
-
-            if mul > 0:
-                cal = table['B'] - mul
-                if  cal >= 0:
-                    table['B'] = cal
-                else:
-                    table['B'] = 0
-
-            result += (table[i] * hashtable[i])
-
-        else:
-            result += (hashtable[i] * table[i])
+        # if i == 'A':
+        #     mul, res = divmod(table[i], 3)
+        #     if mul > 0:
+        #         result += (mul * hashtable[i * 3])
+        #     if res > 0:
+        #         result += (res * hashtable[i])
+        #
+        # elif i == 'B':
+        #     mul, res = divmod(table[i], 2)
+        #     if mul > 0:
+        #         result += (mul * hashtable[i*2])
+        #     if res > 0:
+        #         result += (res * hashtable[i])
+        #
+        # elif i == 'E':
+        #     mul, res = divmod(table[i], 2)
+        #
+        #     if mul > 0:
+        #         cal = table['B'] - mul
+        #         if  cal >= 0:
+        #             table['B'] = cal
+        #         else:
+        #             table['B'] = 0
+        #
+        #     result += (table[i] * hashtable[i])
+        #
+        # else:
+        #     result += (hashtable[i] * table[i])
 
     return result
 
 
 print(checkout('ABBBBBBCDDEEEEEEEEEEEE'))
 
-- {"method": "checkout", "params": ["AAAAA"], "id": "CHK_R2_017"}, expected: 200, got: 230
-- {"method": "checkout", "params": ["AAAAAA"], "id": "CHK_R2_018"}, expected: 250, got: 260
-- {"method": "checkout", "params": ["AAAAAAA"], "id": "CHK_R2_019"}, expected: 300, got: 310
+# - {"method": "checkout", "params": ["AAAAA"], "id": "CHK_R2_017"}, expected: 200, got: 230
+# - {"method": "checkout", "params": ["AAAAAA"], "id": "CHK_R2_018"}, expected: 250, got: 260
+# - {"method": "checkout", "params": ["AAAAAAA"], "id": "CHK_R2_019"}, expected: 300, got: 310
+#
 
 
 
