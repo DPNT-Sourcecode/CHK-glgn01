398f35ae3e3867793f35cff2e95a11bf0f51f70c
sbx_user1051 <sbx_user1051@169.254.34.153>
2022-05-10 17:32:39 +0000

    Tue May 10 17:32:39 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,11 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
+    check_volume = range(100)
+
+    if x not in check_volume and y not in check_volume:
+        raise 
+
+
+
     raise NotImplementedError()
+
