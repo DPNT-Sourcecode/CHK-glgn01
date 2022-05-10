25df0d160fc44b044c7aaddf98fe60dc4ff62c55
sbx_user1051 <sbx_user1051@169.254.34.153>
2022-05-10 18:12:06 +0000

    Tue May 10 18:12:06 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,4 @@
-
+import collections
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -6,6 +6,14 @@ def checkout(skus):
 
     if not isinstance(skus, str):
         return -1
+    
+    if not skus.isupper():
+        return -1
+
+    table = collections.Counter(skus)
+    print(table)
+
+print(checkout('AB*'))
 
 
 
