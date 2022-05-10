dedc169e9673e350effb60ec25c49482ea390a68
sbx_user1051 <sbx_user1051@169.254.34.153>
2022-05-10 22:11:55 +0000

    Tue May 10 22:11:55 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -63,7 +63,7 @@ def checkout(skus):
 
     table = collections.Counter(skus)
 
-    new_event_targets = {'Z','S','T','Y','X'}
+    new_event_targets = {'X','Z','S','T','Y',}
     new_event_table = collections.defaultdict(dict)
     new_event_sum = 0
 
@@ -99,7 +99,7 @@ def checkout(skus):
     if new_event_table:
         result += (new_event_sum // 3) * 45
         new_event_sum -= ((new_event_sum // 3) * 3)
-
+        print(new_event_sum)
         for i in new_event_targets:
 
             if not new_event_table.get(i):
@@ -107,7 +107,7 @@ def checkout(skus):
             if new_event_sum == 0:
                 return result
             cnt, price = hashtable[i][0]
-            
+
             if new_event_table[i] < new_event_sum:
                 new_event_sum -= new_event_table[i]
                 result += (new_event_table[i] * price)
@@ -121,6 +121,7 @@ def checkout(skus):
  # - {"method":"checkout","params":["STXZ"],"id":"CHK_R5_146"}, expected: 62, got: 66
 # new_event_targets = {'S', 'T', 'X', 'Y', 'Z'}
 print(checkout('STXS'))
+
 
 
 
