748a6a00b30c743d88050d4fe69f5c6adadade65
sbx_user1051 <sbx_user1051@169.254.34.153>
2022-05-10 22:01:51 +0000

    Tue May 10 22:01:51 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -114,5 +114,9 @@ def checkout(skus):
                 new_event_sum = 0
 
     return result
-
+ # - {"method":"checkout","params":["SSSZ"],"id":"CHK_R5_142"}, expected: 65, got: 66
+ # - {"method":"checkout","params":["ZZZS"],"id":"CHK_R5_144"}, expected: 65, got: 66
+ # - {"method":"checkout","params":["STXZ"],"id":"CHK_R5_146"}, expected: 62, got: 66
+# new_event_targets = {'S', 'T', 'X', 'Y', 'Z'}
 # print(checkout('FFFXXXXYZZZZZTTSS'))
+
