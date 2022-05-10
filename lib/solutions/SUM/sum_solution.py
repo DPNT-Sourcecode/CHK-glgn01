186c4f9a5df1e397d89bb5fb0c36cbeab1a50fcd
sbx_user1051 <sbx_user1051@169.254.34.153>
2022-05-10 17:38:39 +0000

    Tue May 10 17:38:39 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -3,9 +3,10 @@ def compute(x, y):
     check_volume = range(100)
 
     if x not in check_volume and y not in check_volume:
-        raise 
+        raise Exception("check the value - x : {}, y : {}".format(x, y))
 
+    result = x + y
 
+    return x + y
 
-    raise NotImplementedError()
 
